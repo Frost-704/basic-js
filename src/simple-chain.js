506bf0c6187e105e60816d7f55ed2f6cc092b0a9
Frost-704 <boolean.pg.me@gmail.com>
2023-12-25 00:49:15 +0300

    feat: finish chainMaker task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain : [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (value === null) {
+      value = 'null';
+      }
+    this.chain.push(value)
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (position <= 0 || position > this.chain.length || typeof position !== 'number') {
+      this.chain = [];
+      throw new Error('You can\'t remove incorrect link!');
+      }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  this.chain.reverse()
+  return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let chained = this.chain.map((e) => `( ${e} )`)
+    let result = chained.join('~~');
+    this.chain = [];
+  return result;
   }
 };
 
